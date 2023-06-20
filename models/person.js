ffb6e46939ebc3dbdbb782bf26317a7107e662f3
Henry Palonen <henry.palonen@alsus.fi>
2023-06-20 11:07:11 +0300

    minor

--- models/person.js
+++ models/person.js
@@ -24,7 +24,7 @@ const phonebookSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function(v) {
-        return /\d{3}-\d{7}/.test(v)
+        return /^(\d{3}\s?\d{7})$/.test(v);
       },
       message: props => `${props.value} is not a valid phone number, use 040 1234567!`
     },
